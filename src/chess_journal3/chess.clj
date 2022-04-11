8671de127a388c08bf6a0ca1cfcebded2e88166d
Patrick Waters <patrick.waters@liftoff.io>
2022-04-11 10:41:11 -0700

    Improve chess ns

--- src/chess_journal3/chess.clj
+++ src/chess_journal3/chess.clj
@@ -1,20 +1,31 @@
 (ns chess-journal3.chess
+  "Wraps `bhlangonijr.chesslib`, exposing functions for applying moves to a fen,
+  and checking whether moves are legal. Use of `bhlangonijr.chesslib` is
+  contained within this namespace, so that it would be easy to swap out that
+  library if necessary."
   (:require
     [clojure.string :as string])
   (:import
-    (com.github.bhlangonijr.chesslib Board Piece Square)
-    (com.github.bhlangonijr.chesslib.move MoveList Move)))
+    (com.github.bhlangonijr.chesslib Board Piece Square PieceType)
+    (com.github.bhlangonijr.chesslib.move Move MoveList MoveGenerator)))
 
-(defn apply-move-san [fen san]
-  (let [board (Board.)
-        move-list (MoveList. fen)]
-    (.loadFromFen board fen)
-    (.loadFromSan move-list san)
-    (.doMove board (.removeFirst move-list))
-    (.getFen board)))
+;;-----------------------------------------------------------------------------;;
+;;-------------     Converting between representations of moves      ----------;;
+;;-----------------------------------------------------------------------------;;
 
-(defn get-promote-piece [to-square promote]
-  (let [rank (Integer/parseInt (subs to-square 1 2))]
+(defn- parse-promote-piece [^Move m]
+  (let [^Piece p (.getPromotion m)
+        ^PieceType t (.getPieceType p)]
+    (when t
+      (condp = t
+        PieceType/KNIGHT "N"
+        PieceType/BISHOP "B"
+        PieceType/ROOK "R"
+        PieceType/QUEEN "Q"))))
+
+(defn- ^Piece unparse-promote-piece [move]
+  (let [{:keys [to promote]} move
+        rank (Integer/parseInt (subs to 1 2))]
     (case rank
       8 (case promote
           "N" Piece/WHITE_KNIGHT
@@ -27,29 +38,68 @@
           "R" Piece/BLACK_ROOK
           "Q" Piece/BLACK_QUEEN))))
 
-(defn move-to-san [fen move]
+(defn- object-to-san [fen ^Move m]
+  (let [move-list (MoveList. fen)]
+    (.add move-list m)
+    (first (.toSanArray move-list))))
+
+(defn- san-to-object [fen san]
+  (let [move-list (MoveList. fen)]
+    (.loadFromSan move-list san)
+    (first move-list)))
+
+(defn- object-to-move [^Move m]
+  (let [^Piece p (.getPromotion m)
+        promote-piece (parse-promote-piece m)]
+    (cond-> {:from (str (.getFrom m))
+             :to (str (.getTo m))}
+      promote-piece (assoc :promote promote-piece))))
+
+(defn- move-to-object [move]
   (let [{:keys [to
                 from
                 promote]} move
-        move-list (MoveList. fen)
         from-square (Square/valueOf (string/upper-case from))
         to-square (Square/valueOf (string/upper-case to))
         promotePiece (when promote
-                       (get-promote-piece to promote))
-        move (if promote
-               (Move. from-square to-square promotePiece)
-               (Move. from-square to-square))]
-    (.add move-list move)
-    (.toSan move-list)))
+                       (unparse-promote-piece move))]
+    (if promote
+      (Move. from-square to-square promotePiece)
+      (Move. from-square to-square))))
+
+(defn move-to-san [fen move]
+  (->> (move-to-object move)
+       (object-to-san fen)))
+
+(defn san-to-move [fen san]
+  (object-to-move
+    (san-to-object fen san)))
+
+;;-----------------------------------------------------------------------------;;
+;;-------------                   Applying moves                     ----------;;
+;;-----------------------------------------------------------------------------;;
+
+(defn apply-move-san [fen san]
+  (let [board (Board.)
+        move-list (MoveList. fen)]
+    (.loadFromFen board fen)
+    (.loadFromSan move-list san)
+    (.doMove board (first move-list))
+    (.getFen board)))
 
 (defn apply-move [fen move]
   (let [san (move-to-san fen move)]
     (apply-move-san fen san)))
 
-(defn san-to-move [fen san]
-  (let [board (Board.)
-        move-list (MoveList. fen)]
+;;-----------------------------------------------------------------------------;;
+;;-------------               Legal moves / checking                 ----------;;
+;;-----------------------------------------------------------------------------;;
+
+(defn get-legal-move-sans [fen]
+  (let [board (Board.)]
     (.loadFromFen board fen)
-    (.loadFromSan move-list san)
-    {:from (str (.getFrom (first move-list)))
-     :to (str (.getTo (first move-list)))}))
+    (->> (MoveGenerator/generateLegalMoves board)
+         (map (partial object-to-san fen)))))
+
+(defn legal-move-san? [fen san]
+  (contains? (set (get-legal-move-sans fen)) san))
--- test/chess_journal3/chess_test.clj
+++ test/chess_journal3/chess_test.clj
@@ -1,21 +1,69 @@
 (ns chess-journal3.chess-test
   (:require
     [chess-journal3.chess :as chess]
-    [clojure.test :refer [is deftest]]))
+    [clojure.test :refer [is deftest]])
+  (:import
+    (com.github.bhlangonijr.chesslib Square Piece)
+    (com.github.bhlangonijr.chesslib.move Move)))
+
+(def initial-fen "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+(def fen-after-1e4 "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1")
+
+(deftest parsing-promote-piece
+  (is (= "N"
+         (#'chess/parse-promote-piece (Move. (Square/valueOf "E7")
+                                             (Square/valueOf "E8")
+                                             Piece/WHITE_KNIGHT))))
+  (is (nil? (#'chess/parse-promote-piece (Move. (Square/valueOf "E2")
+                                                (Square/valueOf "E4"))))))
+
+(deftest unparsing-promote-piece
+  (is (= Piece/WHITE_KNIGHT
+         (#'chess/unparse-promote-piece {:to "E8" :promote "N"}))))
+
+(deftest converting-object-to-san
+  (is (= "e4"
+         (#'chess/object-to-san initial-fen
+          (Move. (Square/valueOf "E2")
+                 (Square/valueOf "E4"))))))
+
+(deftest converting-san-to-object
+  (is (= (Move. (Square/valueOf "E2")
+                (Square/valueOf "E4"))
+         (#'chess/san-to-object initial-fen "e4"))))
+
+(deftest converting-object-to-move
+  (is (= {:from "E2" :to "E4"}
+         (#'chess/object-to-move (Move. (Square/valueOf "E2")
+                                        (Square/valueOf "E4"))))))
+
+(deftest converting-move-to-object
+  (is (= (Move. (Square/valueOf "E2")
+                (Square/valueOf "E4"))
+         (#'chess/move-to-object {:from "E2" :to "E4"}))))
+
+(deftest converting-move-to-san
+  (is (= "e4"
+         (chess/move-to-san initial-fen {:from "e2" :to "e4"}))))
+
+(deftest converting-san-to-move
+  (is (= {:from "E2" :to "E4"}
+         (chess/san-to-move initial-fen "e4"))))
 
 (deftest applying-move-san
-  (is (= "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
-         (chess/apply-move-san "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-                               "e4"))))
+  (is (= fen-after-1e4
+         (chess/apply-move-san initial-fen "e4"))))
 
 (deftest applying-move
-  (is (= "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
-         (chess/apply-move "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-                           {:from "e2"
-                            :to "e4"}))))
+  (is (= fen-after-1e4
+         (chess/apply-move initial-fen
+                           {:from "e2" :to "e4"}))))
 
-(deftest changing-san-to-move
-  (is (= {:from "E2"
-          :to "E4"}
-         (chess/san-to-move "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-                            "e4"))))
+(deftest getting-legal-move-sans
+  (is (= ["a3" "a4" "b3" "b4" "c3" "c4" "d3" "d4"
+          "e3" "e4" "f3" "f4" "g3" "g4" "h3" "h4"
+          "Na3" "Nc3" "Nf3" "Nh3"]
+         (chess/get-legal-move-sans initial-fen))))
+
+(deftest checking-if-move-is-legal
+  (is (chess/legal-move-san? initial-fen "e4")))
